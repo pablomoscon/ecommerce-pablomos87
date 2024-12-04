766513a2f4aa6f5e58a46ff688648f3920ca93e6
pablomos <mosconpablo@gmail.com>
2024-12-04 07:47:26 -0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { DataSourceInstance } from './config/database.config';
 import { MigrationExecutor } from 'typeorm';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await DataSourceInstance.initialize();
 
-   await DataSourceInstance.initialize();
   const migrationExecutor = new MigrationExecutor(DataSourceInstance);
   const executedMigrations = await migrationExecutor.getExecutedMigrations();
+
   if (executedMigrations.length === 0) {
     await DataSourceInstance.runMigrations();
-<<<<<<< HEAD
   }
-=======
-  };
->>>>>>> 60a5620a069b3e7d0a2a6581bfe4a888c37e0587
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -32,6 +27,7 @@ async function bootstrap() {
   }));
   app.use(loggerGloblal);
 
+  // Swagger y Seeds
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Demo Nest')
     .setDescription('Esta es una API construida con Nest para ser empleada en las demos del módulo 4 de la especialidad backend de la carrera Fullstack Developer')
@@ -39,15 +35,10 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
-
-<<<<<<< HEAD
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
-=======
-const document = SwaggerModule.createDocument(app, swaggerConfig);
-SwaggerModule.setup('docs', app, document);
->>>>>>> 60a5620a069b3e7d0a2a6581bfe4a888c37e0587
 
+  // Seeds
   const categoriesSeed = app.get(CategoriesSeed);
   await categoriesSeed.seed();
   console.log('The insertion of categories has been completed.');
